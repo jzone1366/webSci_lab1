12ffb7821f4a1bb46b368a4a5b74931ea8299151
zonej <jzone1229@gmail.com>
2014-02-01 01:00:30 -0500

    Finished js for Tweets feed. Added basic styling. Need to add grid Foundation and create hashtag feed.

--- js/tweet.js
+++ js/tweet.js
@@ -3,12 +3,26 @@ $.getJSON("resources/tweets-clean.json", function(data) {
 	var tweets = [];
 	var hashes = [];
 	$.each(myData, function(i, item) {
-		//console.log(item.text);
-		
+		tweets.push("<li>" + item.text + "</li>");
 		if(item.entities.hashtags[0] != null) {
 			for(var j = 0; j < item.entities.hashtags.length; j++) {
-				//console.log("#" + item.entities.hashtags[j].text);
+				hashes.push("<li>#" + item.entities.hashtags[j].text + "</li>");
 			}
 		}
-	})
+		$('ul').html(tweets.join(''));
+		//$('ul').html(hashes.join(''));
+	});
 });
+function tweet_slide() {
+	$('#tweet-ticker li:first').slideUp(function() {
+		$(this).appendTo($('#tweet-ticker')).slideDown();
+	});
+};
+setInterval(function() { tweet_slide() }, 3000);
+
+/*function hash_slide() {
+	$('#hash-ticker li:first').slideUp(function() {
+		$(this).appendTo($('#hash-ticker')).slideDown();
+	});
+};
+setInterval(function() {hash-slide() }, 3000);*/
